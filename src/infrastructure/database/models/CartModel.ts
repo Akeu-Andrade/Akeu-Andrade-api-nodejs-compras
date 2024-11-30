c618bb38a056330d78fe22749dae2fecdfc24db1
Akeu-Andrade <akeusantana@gmail.com>
2024-11-30 14:51:41 -0300

    feat: refatora CartModel para detalhes de product

--- src/infrastructure/database/models/CartModel.ts
+++ src/infrastructure/database/models/CartModel.ts
@@ -1,16 +1,26 @@
 import mongoose, { Schema, Document } from 'mongoose';
 import { Product } from '../../../domain/entities/Product';
 
+interface IProductCartModel {
+    productId: string;
+    quantity: number;
+    price: number;
+}
+
 interface ICartModel extends Document {
     userId: string;
-    products: Product[];
+    products: IProductCartModel[];
     createdAt: Date;
     updatedAt: Date;
 }
 
 const CartSchema = new Schema({
     userId: { type: String, required: true },
-    products: { type: Array, required: true },
+    products: [{
+        productId: { type: String, required: true },
+        quantity: { type: Number, required: true },
+        price: { type: Number, required: true }
+    }],
     createdAt: { type: Date, required: true },
     updatedAt: { type: Date, required: true }
 });
