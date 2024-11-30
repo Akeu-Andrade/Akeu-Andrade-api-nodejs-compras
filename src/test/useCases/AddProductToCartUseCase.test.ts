8fb1ad5ba8183f43b071a5f7233280796b3d911f
Akeu-Andrade <akeusantana@gmail.com>
2024-11-30 17:37:09 -0300

    feat: update README com descricao de instalacao

---
+++ src/test/useCases/AddProductToCartUseCase.test.ts
@@ -0,0 +1,20 @@
+import { before, describe, it } from "node:test";
+
+describe("AddProductToCartUseCase", () => {
+
+    it("deve adicionar um novo produto ao carrinho caso ele não exista", async () => {
+
+    });
+
+    it("deve atualizar a quantidade se o produto já estiver no carrinho", async () => {
+
+    });
+    
+    it("deve lançar erro se o carrinho não for encontrado", async () => {
+
+    });
+
+    it("deve lançar erro se o produto não for encontrado", async () => {
+
+    });
+});
